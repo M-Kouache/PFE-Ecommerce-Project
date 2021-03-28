96fedddc38c5964145cb8fea0b184808ab0db38e
Mohamed Kouache <mkouache27@gmail.com>
2021-03-28 19:00:30 +0100

    adding a banner under product section

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,12 @@
+let banner_img = document.querySelector(".banner-image");
+let window_width = window.innerWidth;
 
+
+if (window_width > 900) {
+
+    banner_img.src = "assets/images/banner-desktop.png"
     
+}
+
     
 
