a457f5ee2da503626f81c42fc090249cf4414779
Mohamed Kouache <mkouache27@gmail.com>
2021-03-29 18:32:25 +0100

    light changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,25 @@
 let banner_img = document.querySelector(".banner-image");
+let gl_img_contianer = document.querySelectorAll(".gl-img-container");
+let gl_collection_title = document.querySelectorAll(".gl-title")
 let window_width = window.innerWidth;
 
 
 if (window_width > 900) {
-
     banner_img.src = "assets/images/banner-desktop.png"
-    
 }
 
+
+gl_img_contianer.forEach(element => {
+    element.onmouseover = function() {
+        element.style.background = "none"
+        element.children[1].style.visibility = "hidden"
+    }
     
+    element.onmouseout = function() {
+        element.style.background = "rgba(0, 0, 0, 0.8)"
+        element.children[1].style.visibility = "visible"
+    }
+});
+
+
 
