5744699fefaa75d5988be7fc4aca5e6fbe89f090
Mohamed Kouache <mkouache27@gmail.com>
2021-05-26 15:35:08 +0100

    adding pages like : login/regester/forgetpassword/productDeatail with styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,6 +2,27 @@ let banner_img = document.querySelector(".banner-image");
 let gl_img_contianer = document.querySelectorAll(".gl-img-container");
 let gl_collection_title = document.querySelectorAll(".gl-title")
 let window_width = window.innerWidth;
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
 
 
 if (window_width > 900) {
